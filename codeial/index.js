42afede8030634720d73a3b71f77fe994955b728
sahilverma3120 <62200238+sahilverma3120@users.noreply.github.com>
2021-02-16 23:47:54 +0530

    Update index.js

--- codeial/index.js
+++ codeial/index.js
@@ -2,13 +2,18 @@ const express = require('express');
 const app = express();
 const port = 8000;
 
+// use express router
+app.use('/', require('./routes'));
+
+// set up the view engine
+app.set('view engine', 'ejs');
+app.set('views', './views');
 
 
-//to use the express router
-app.use('/',require('./routes'));
-app.listen(port,function(err)){
-if(err){
-console.log(`Error in running the server`, $(err)`};
-}
-console.log(`Error in running the serveer, $(port)`};
+app.listen(port, function(err){
+    if (err){
+        console.log(`Error in running the server: ${err}`);
+    }
+
+    console.log(`Server is running on port: ${port}`);
 });
