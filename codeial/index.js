d022a568f178eadcb9fdb3e652de20213bf3f5c8
sahilverma3120 <62200238+sahilverma3120@users.noreply.github.com>
2021-02-16 23:05:34 +0530

    Create index.js

---
+++ codeial/index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+const port = 8000;
+
+app.listen(port,function(err)){
+if(err){
+console.log(`Error in running the serveer`, $(err)};
+}
+console.log(`Error in running the serveer`, $(port)};
+});
