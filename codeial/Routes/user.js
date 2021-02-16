7409f6399e1761a62bf7621cf6fab5e4813f810a
sahilverma3120 <62200238+sahilverma3120@users.noreply.github.com>
2021-02-16 23:44:30 +0530

    Update user.js

--- codeial/Routes/user.js
+++ codeial/Routes/user.js
@@ -3,4 +3,6 @@ const router = express.Router();
 const userController = require('../controllers/user_controller');
 router.get('/',homeController.home);
 router.use('./users',require('./users'));
+// for anty further routes, access from here
+// router.use('/routerName', require('./routerfile));
 modules.exports = router;
