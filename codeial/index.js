4b83846df2da0e06b36d11b46851352b8f0b1e2d
sahilverma3120 <62200238+sahilverma3120@users.noreply.github.com>
2021-02-19 18:24:28 +0530

    Update index.js

--- codeial/index.js
+++ codeial/index.js
@@ -1,8 +1,17 @@
 const express = require('express');
+const cookieParser = require('cookie-parser');
 const app = express();
 const port = 8000;
 const expressLayouts = require('express-ejs-layouts');
 const db = require('./config/mongoose');
+// used for session cookie
+const session = require('express-session');
+const passport = require('passport');
+const passportLocal = require('./config/passport-local-strategy');
+
+app.use(express.urlencoded());
+
+app.use(cookieParser());
 
 app.use(express.static('./assets'));
 
@@ -12,12 +21,29 @@ app.set('layout extractStyles', true);
 app.set('layout extractScripts', true);
 
 
-// use express router
-app.use('/', require('./routes'));
+
 
 // set up the view engine
 app.set('view engine', 'ejs');
 app.set('views', './views');
+
+
+app.use(session({
+    name: 'codeial',
+    // TODO change the secret before deployment in production mode
+    secret: 'blahsomething',
+    saveUninitialized: false,
+    resave: false,
+    cookie: {
+        maxAge: (1000 * 60 * 100)
+    }
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+// use express router
+app.use('/', require('./routes'));
 
 
 app.listen(port, function(err){
