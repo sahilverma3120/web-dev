e233814bad0f200340611cc63f64c43e9dbb6750
sahilverma3120 <62200238+sahilverma3120@users.noreply.github.com>
2021-03-22 23:47:15 +0530

    Create Index.js

---
+++ Major Project 2/Index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const port = 8000;
+const expressLayouts = require('express-ejs-layouts');
+
+app.use(expressLayouts);
+
+// use express router
+app.use('/', require('./routes'));
+
+// set up the view engine
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+
+app.listen(port, function(err){
+    if (err){
+        console.log(`Error in running the server: ${err}`);
+    }
+
+    console.log(`Server is running on port: ${port}`);
+});
